d69ed7abd22ce8f694aa9a95e2e90e5cbae93553
jts159753 <jts159753@snu.ac.kr>
2021-01-14 12:32:54 +0900

    post_list READ

--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 
 def post_list(request):
-    return HttpResponse('글 리스트')
+    """
+    Read(R)
+    포스트들을 불러와서 리스트 형태로 보여준다.
+    """
+
+    posts = Post.objects.all()
+    ctx = {'posts': posts}
+    return render(request, template_name='posts/list.html', context=ctx)
 # Create your views here.
